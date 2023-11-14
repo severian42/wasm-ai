071ac018d24604b40c802c42708a5fb62e0fd2ff
Beckett <133655553+severian42@users.noreply.github.com>
2023-11-14 09:42:27 -0500

    Update supported-models.ts

--- src/lib/wasmllm/supported-models.ts
+++ src/lib/wasmllm/supported-models.ts
@@ -3,24 +3,14 @@ import { WebGPUModel } from './wasmllm';
 export const SUPPORTED_LOCAL_MODELS: {
   [key: string]: WebGPUModel;
 } = {
-  "dolphin-2.2.1": {
-    simpleName: "Dolphin 2.2.1",
-    modelName: "dolphin-2.2.1-mistral-7b-q4f32_1",
+  "ANIMA": {
+    simpleName: "ANIMA",
+    modelName: "ANIMA-Phi-Neptune-Mistral-7B-q4f32_1",
     rootUrl:
-      "https://huggingface.co/hrishioa/mlc-chat-dolphin-2.2.1-mistral-7b-q4f32_1",
+      "https://huggingface.co/hrishioa/wasm-ANIMA-Phi-Neptune-Mistral-7B-q4f32_1",
     modelParamsUrl:
-      "https://huggingface.co/hrishioa/mlc-chat-dolphin-2.2.1-mistral-7b-q4f32_1/resolve/main/params/",
+      "https://huggingface.co/hrishioa/wasm-ANIMA-Phi-Neptune-Mistral-7B-q4f32_1/resolve/main/params/",
     wasmUrl:
-      "https://huggingface.co/hrishioa/mlc-chat-dolphin-2.2.1-mistral-7b-q4f32_1/resolve/main/dolphin-2.2.1-mistral-7b-q4f32_1-webgpu.wasm",
-  },
-  "openhermes-2.5": {
-    simpleName: "OpenHermes 2.5",
-    modelName: "OpenHermes-2.5-Mistral-7B-q4f32_1",
-    rootUrl:
-      "https://huggingface.co/hrishioa/wasm-OpenHermes-2.5-Mistral-7B-q4f32_1",
-    modelParamsUrl:
-      "https://huggingface.co/hrishioa/wasm-OpenHermes-2.5-Mistral-7B-q4f32_1/resolve/main/params/",
-    wasmUrl:
-      "https://huggingface.co/hrishioa/wasm-OpenHermes-2.5-Mistral-7B-q4f32_1/resolve/main/OpenHermes-2.5-Mistral-7B-q4f32_1-webgpu.wasm",
+      "https://huggingface.co/hrishioa/wasm-ANIMA-Phi-Neptune-Mistral-7B-q4f32_1/resolve/main/ANIMA-Phi-Neptune-Mistral-7B-q4f32_1-webgpu.wasm",
   },
 };
